4e302082d6409ae5a0e7ef5eccd7a93dd7b0fbad
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-18 10:08:20 +0100

    Add files via upload

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -8,18 +8,22 @@ export interface IAppContextInterface {
   };
 }
 
-const ctxt = React.createContext<IAppContextInterface | null>(null);
 
-export const AppContextProvider = ctxt.Provider;
+const context = React.createContext<IAppContextInterface | null>(null);
 
-export const AppContextConsumer = ctxt.Consumer;
+export const AppContextProvider = context.Provider;
 
-export function withAppContext<P extends { appContext?: IAppContextInterface }>(Component: React.ComponentClass<P> | React.FunctionComponent<P>
-): React.FunctionComponent<P> {
-  return function BoundComponent(props: P) {
+export const AppContextConsumer = context.Consumer;
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+export function withAppContext<P extends { appContext?: IAppContextInterface },R = Omit<P, "appContext">>(
+    WrappedComponent: React.ComponentClass<P> | React.FunctionComponent<P>
+  ): React.FunctionComponent<R> {
+  return function BoundComponentHOC(props: R) {
     return (
       <AppContextConsumer>
-        {value => <Component {...props} appContext={value} />}
+        {value => <WrappedComponent {...props as any} appContext={value} />}
       </AppContextConsumer>
     );
   };
--- src/PostInfo.tsx
+++ src/PostInfo.tsx
@@ -1,13 +1,31 @@
 import * as React from "react";
-import { withAppContext } from "./AppContext";
+import { IAppContextInterface, withAppContext } from "./AppContext";
 
-const PostInfoComp = ({ appContext }: any) =>
+
+/* Option 1: Function component */
+/*const PostInfoComp = ({ appContext }: { appContext?: IAppContextInterface }) =>
   appContext && (
     <div>
-      lang: {appContext.lang} <br />
-      chat: {appContext.nav.chatTabLabel} <br />
-      settings: {appContext.nav.settingsTabLabel}
+      Language: {appContext.lang} <br />
+      Chat tab: {appContext.nav.chatTabLabel} <br />
+      Settings tab: {appContext.nav.settingsTabLabel}
     </div>
   );
+
+export const PostInfo = withAppContext(PostInfoComp as React.FunctionComponent);*/
+
+/* Option 2: Class component */
+export class PostInfoComp extends React.Component<{ appContext?: IAppContextInterface }, {}> {
+  public render() {
+    const { appContext } = this.props;
+    return (
+      appContext && <div>
+            Language: {appContext.lang} <br />
+            Chat tab: {appContext.nav.chatTabLabel} <br />
+            Settings tab: {appContext.nav.settingsTabLabel}
+          </div>
+    );
+  }
+}
 
 export const PostInfo = withAppContext(PostInfoComp);
